a97ac37449ba522668af151d7ef4584d6ae2ac0c
kalandarisvili <a.kalandarisvili@gmail.com>
2020-10-30 15:10:03 +0100

    first commit to the CMS development branch

--- components/layouts/Main.jsx
+++ components/layouts/Main.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import AppBar from "../common/AppBar";
 import { Container } from "@material-ui/core";
 import Footer from "../common/Footer";
-import Head from "next/head";
 
 export default function Main({ children, ...pageProps }) {
   return (
